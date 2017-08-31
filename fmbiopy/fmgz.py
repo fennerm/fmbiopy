ef9f7d710813d273f05c1d8e98885096badb57e1
Fenner Macrae <schaack@watson.ibest.uidaho.edu>
2017-08-31 16:56:29 -0700

    Added small test for fmlog.setup_log

--- fmbiopy/fmgz.py
+++ fmbiopy/fmgz.py
@@ -1,18 +1,15 @@
 import os
+from pathlib import Path
 
-# Gunzip a given path 'p' into 'out_dir', and keep 'p'
 def gunzip(p, out_dir):
+    """ Gunzip a file and output to out_dir. Keep the original file"""
     unzip_name = str(p.name)[:-3]
-
-    if unzip_name.endswith('.fq'):
-        unzip_name = unzip_name[:-2] + 'fastq'
     unzip_path = Path(out_dir, unzip_name)
     os.system('gunzip -c '+ str(p) + ' > ' + str(unzip_path))
     return unzip_path
 
-# Gzip a file
 def gzip(f):
+    """ Gzip a file """
     os.system('gzip ' + str(f))
     renamed = Path(str(f) + '.gz')
     return renamed
-
---
+++ test/test_setup_log.py
@@ -0,0 +1,16 @@
+import logging, pytest, tempfile
+from tempfile import NamedTemporaryFile
+from fmbiopy.fmlog import setup_log
+
+def gen_tmp_logfile():
+    tmp = NamedTemporaryFile()
+    setup_log(tmp.name)
+    return tmp
+
+def test_root_logging():
+    """ Test that logging.info writes a line to the logfile""" 
+
+    logfile = gen_tmp_logfile()
+    logging.info("foo")
+    line_count = sum(1 for line in open(logfile.name))
+    assert line_count == 1
