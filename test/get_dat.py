4183ef97e912e16bde03d0bd3c102d3e8b17624c
Fenner Macrae <schaack@crick.ibest.uidaho.edu>
2017-09-03 14:16:38 -0700

    Updated get_dat to ignore non-fasta files in assemblies dir

--- test/get_dat.py
+++ test/get_dat.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python
-
 import os
 from glob import glob
+
 def get_dat():
     dat = {
         'fwd_reads' : sorted([os.path.abspath(x) for x in
-                              glob('testdat/reads/fwd/*')]),
+                              glob('testdat/reads/fwd/*.fq.gz')]),
         'rev_reads' : sorted([os.path.abspath(x) for x in
-                              glob('testdat/reads/rev/*')]),
+                              glob('testdat/reads/rev/*.fq.gz')]),
         'assemblies' : sorted([os.path.abspath(x) for x in
-                               glob('testdat/assembly/*fa*')])
+                               glob('testdat/assembly/*.fasta')])
     }
     return dat
