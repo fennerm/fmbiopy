f3cd1ec79f2edba88ef7da025dbdb3f229978980
Fenner Macrae <schaack@watson.ibest.uidaho.edu>
2017-09-05 14:37:20 -0700

    Updated test infrastructure
    Any files placed in testdat will now be automatically detected by get_dat

--- test/get_dat.py
+++ test/get_dat.py
@@ -1,12 +1,11 @@
 import os
 from glob import glob
-from fmbiopy.fmpaths import abs_paths
+from fmbiopy.fmpaths import abs_paths, listdirs
 
 def get_dat():
-    dat = {
-        'fwd_reads' : sorted(abs_paths(glob('testdat/reads/fwd/*.fq.gz'))),
-        'rev_reads' : sorted(abs_paths(glob('testdat/reads/rev/*.fq.gz'))),
-        'assemblies' : sorted(abs_paths(glob('testdat/assembly/*.fasta'))),
-        'empty' : sorted(abs_paths(glob('testdat/reads/empty/*.fq')))
-    }
+    testdirs = abs_paths(listdirs('testdat'))
+    dat = {}
+    for d in testdirs:
+        base = os.path.basename(d)
+        dat[base] = sorted(glob(d + '/*'))
     return dat
