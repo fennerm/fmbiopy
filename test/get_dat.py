c9f1035a5c548bd9edce8ca7e400fc503980aa1b
Fenner Macrae <schaack@petunia.ibest.uidaho.edu>
2017-09-04 19:33:33 -0700

    Added empty files for testing

--- test/get_dat.py
+++ test/get_dat.py
@@ -1,13 +1,12 @@
 import os
 from glob import glob
+from fmbiopy.fmpaths import abs_paths
 
 def get_dat():
     dat = {
-        'fwd_reads' : sorted([os.path.abspath(x) for x in
-                              glob('testdat/reads/fwd/*.fq.gz')]),
-        'rev_reads' : sorted([os.path.abspath(x) for x in
-                              glob('testdat/reads/rev/*.fq.gz')]),
-        'assemblies' : sorted([os.path.abspath(x) for x in
-                               glob('testdat/assembly/*.fasta')])
+        'fwd_reads' : sorted(abs_paths(glob('testdat/reads/fwd/*.fq.gz'))),
+        'rev_reads' : sorted(abs_paths(glob('testdat/reads/rev/*.fq.gz'))),
+        'assemblies' : sorted(abs_paths(glob('testdat/assembly/*.fasta'))),
+        'empty' : sorted(abs_paths(glob('testdat/reads/empty/*.fq')))
     }
     return dat
