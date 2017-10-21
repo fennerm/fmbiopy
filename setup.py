27dc30ec3d69bc690c331668fe2a9526914980b0
Fenner Macrae <fennermacrae@gmail.com>
2017-10-21 15:54:35 -0700

    Module is now a package (Closes #6)

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import (
+        find_packages,
+        setup,
+        )
 
 setup(name='fmbiopy',
       version='0.1',
-      description="""Misc. personal bioinformatics python modules which I share 
+      description="""Misc. personal bioinformatics python modules which I share
                      between projects""",
       url='http://github.com/fennerm/fmbiopy',
       author='Fenner Macrae',
       author_email='fennermacrae@gmail.com',
       license='MIT',
-      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", 
-                                      "tests"]),
+      packages=find_packages(exclude=["*test*"]),
       zip_safe=False)
