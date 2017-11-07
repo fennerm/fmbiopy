eb951cfc6462d1b93547b1840976d119c5d37300
Fenner Macrae <fennermacrae@gmail.com>
2017-11-06 17:55:31 -0800

    Updated setup script

--- setup.py
+++ setup.py
@@ -1,13 +1,48 @@
+"""Setup script"""
+from pathlib import Path
 from setuptools import (
         find_packages,
         setup,
         )
 
-setup(name='fmbiopy',
-      version='0.1',
-      description="""Misc. personal bioinformatics python modules which I share
-                     between projects""",
-      url='http://github.com/fennerm/fmbiopy',
+# =============================================================================
+# Globals
+# =============================================================================
+"""Location of the README file"""
+README = Path('README.md')
+
+"""Github username"""
+USERNAME = 'fennerm'
+
+"""Package name"""
+NAME = 'fmbiopy'
+
+
+# =============================================================================
+# Helpers
+# =============================================================================
+
+
+def long_description(readme: Path = README)-> str:
+    """Extract the long description from the README"""
+    try:
+        from pypandoc import convert
+        long_description = convert(str(readme), 'md', 'rst')
+    except (ImportError, IOError, OSError):
+        long_description = readme.read_text()
+    return long_description
+
+
+def url(name: str = NAME, username: str = USERNAME)-> str:
+    """Generate the package url from the package name"""
+    return '/'.join(['http://github.com', username, name])
+
+
+setup(name=NAME,
+      version='0.0.1',
+      description=long_description()[0],
+      long_description=long_description(),
+      url=url(),
       author='Fenner Macrae',
       author_email='fennermacrae@gmail.com',
       license='MIT',
