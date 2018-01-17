376c45c4ea52f434efade39760315060f7686280
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-01-17 10:43:04 -0800

    optget: grab a single option from command line arguments

--- fmbiopy/fmparse.py
+++ fmbiopy/fmparse.py
@@ -1,25 +1,71 @@
 """Parsing utilities"""
 from collections import defaultdict
 import sys
-from typing import (
-        Any,
-        Dict,
-        List,
-        Sequence,
-        Tuple,
-        Union,
-        )
 
 from docopt import (
         docopt,
         DocoptExit,
         )
 
-def helpful_docopt(doc: str, *args, **kwargs)-> Dict[str, Any]:
-    """Wrapper around docopt which prints the full usage message upon error"""
+def helpful_docopt(doc, *args, **kwargs):
+    """Wrapper around docopt which prints the full usage message upon error
+
+    Parameters
+    ----------
+    doc: str
+        Docstring to parse
+
+    Returns
+    -------
+    Dict[str, Union[str, List[str]]]
+        Same output as normal docopt
+
+    """
     try:
         opt = docopt(doc, help=False, *args, **kwargs)
         return opt
     except DocoptExit:
         print(doc)
         sys.exit()
+
+
+def optget(args, short, long=None):
+    """Parse command line arguments to fetch the value of an option
+
+    Parameters
+    ----------
+    args: List[str]
+        Command line arguments
+    short: str
+        The short name of the option (including dash)
+    long: str, optional
+        The long name of the option (including dashes)
+
+    Returns
+    -------
+    str
+        The option value.
+
+    Raises
+    ------
+    ValueError
+        If option is not present in args
+    """
+    try:
+        # Try parse as a short option
+        opt = args[args.index(short) + 1]
+    except ValueError:
+        if long:
+            # Try parse as a long option
+            for arg in args:
+                if arg.startswith(long):
+                    if arg == long:
+                        opt = args[args.index(arg) + 1]
+                    else:
+                        opt = arg.split("=")[1]
+            try:
+                opt
+            except NameError:
+                raise ValueError("Opt not present in args")
+        raise
+    return opt
