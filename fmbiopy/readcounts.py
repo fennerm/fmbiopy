5599b2053a379ad275e16e2b946163e620e3276a
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-05-30 14:11:41 -0700

    Some utilities for parsing mpileup2readcounts output

---
+++ fmbiopy/readcounts.py
@@ -0,0 +1,118 @@
+"""Utilities for working with readcount files.
+
+At present most functions are designed to work with output of IARCbioinfo's
+fork of mpileup2readcounts (github.com/IARCbioinfo/mpileup2readcounts), but
+other data formats might be added later.
+"""
+from collections import OrderedDict
+
+from numpy import NaN
+from pandas import concat, DataFrame, isnull, Series
+
+from fmbiopy.fmlist import pairwise_intersect
+
+from pandas import read_csv
+
+# pileups = [
+#     "/home/fen-arch/fmacrae/megadaph.private/megadaph/pipe/tmp/FA10.head.pileup",
+#     "/home/fen-arch/fmacrae/megadaph.private/megadaph/pipe/tmp/FA7.head.pileup",
+#     "/home/fen-arch/fmacrae/megadaph.private/megadaph/pipe/tmp/FAEC1.head.pileup",
+# ]
+#
+# per_sample_counts = [read_csv(pileup, sep="\t") for pileup in pileups]
+# per_sample_counts = [destrand_counts(counts) for counts in per_sample_counts]
+
+
+def combine_indel_counts(indel_cell):
+    """Combine indel counts from a cell in a count table."""
+    if isnull(indel_cell):
+        return NaN
+    indel_counts = OrderedDict()
+    for indel in indel_cell.split("|"):
+        indel_count, indel_type = tuple(indel.split(":"))
+        try:
+            indel_counts[indel_type.upper()] += int(indel_count)
+        except KeyError:
+            indel_counts[indel_type.upper()] = int(indel_count)
+    return "|".join([":".join([str(v), k]) for k, v in indel_counts.items()])
+
+
+def destrand_counts(counts):
+    """Combine counts in table across strands.
+
+    Parameters
+    ----------
+    counts: DataFrame
+        mpileup2readcounts output as a pandas DataFrame
+
+    Returns
+    -------
+    DataFrame
+
+    """
+    combined_counts = counts[["chr", "loc", "ref", "depth"]].copy()
+    for i, nuc in enumerate(["A", "T", "C", "G"]):
+        combined_counts[nuc] = counts[nuc] + counts[nuc.lower()]
+    for indel_type in ["Insertion", "Deletion"]:
+        combined_counts[indel_type] = counts[indel_type].apply(
+            combine_indel_counts
+        )
+    return combined_counts
+
+
+def find_shared_bases(per_sample_counts, nucleotide):
+    nsamples = len(per_sample_counts)
+    base_counts = concat(
+        [counts[nucleotide] for counts in per_sample_counts], axis=1
+    )
+    return ((base_counts == 0).sum(axis=1) < (nsamples - 1)).values
+
+
+def _get_indel_set(indel_cell):
+    """Get the indel alleles present in a readcount cell as a set."""
+    if isnull(indel_cell):
+        indel_set = set()
+    else:
+        indel_set = set(
+            [indel.split(":")[1] for indel in indel_cell.split("|")]
+        )
+    return indel_set
+
+
+def _get_shared_indels_from_row(row):
+    """Get indels which are present in multiple samples from row of indel
+    readcount table.
+    """
+    indel_sets = [_get_indel_set(cell) for cell in row.values]
+    shared_indels = pairwise_intersect(indel_sets)
+    if shared_indels:
+        return list(shared_indels)
+    else:
+        return NaN
+
+
+def find_shared_indels(per_sample_counts, allele):
+    """Find all indel variants which are not unique to a sample.
+
+    Parameters
+    ----------
+    per_sample_counts: List[DataFrame]
+        List of unstranded count tables. One for each sample.
+    allele: str
+        One of ["Insertion", "Deletion"]
+
+    Returns
+    -------
+    DataFrame
+        Return a DataFrame containing shared indels and their indices.
+
+    """
+    indel_counts = concat(
+        [counts[allele] for counts in per_sample_counts], axis=1
+    )
+    mult_samples_have_indels = indel_counts.notnull().sum(axis=1) > 1
+    mult_indel_counts = indel_counts[mult_samples_have_indels]
+    shared_indels = mult_indel_counts.apply(
+        _get_shared_indels_from_row, axis=1, result_type="reduce"
+    ).dropna()
+    return shared_indels
---
+++ test/test_readcounts.py
@@ -0,0 +1,468 @@
+from collections import OrderedDict
+
+import numpy as np
+from pandas import DataFrame
+from pytest import fixture, mark
+
+from fmbiopy.readcounts import *
+
+
+@fixture
+def stranded_counts():
+    return DataFrame(
+        OrderedDict(
+            (
+                ("chr", ["scaf1", "scaf2"]),
+                ("loc", [1, 2]),
+                ("ref", ["G", "A"]),
+                ("depth", [16, 14]),
+                ("A", [1, 0]),
+                ("T", [0, 2]),
+                ("C", [2, 0]),
+                ("G", [3, 1]),
+                ("a", [1, 0]),
+                ("t", [2, 3]),
+                ("c", [1, 0]),
+                ("g", [1, 2]),
+                ("Insertion", ["2:AA|1:aa", "1:C|2:T"]),
+                ("Deletion", [np.NaN, "1:AC|2:ac"]),
+            )
+        )
+    )
+
+
+@fixture
+def unstranded_counts():
+    return DataFrame(
+        OrderedDict(
+            (
+                ("chr", ["scaf1", "scaf2"]),
+                ("loc", [1, 2]),
+                ("ref", ["G", "A"]),
+                ("depth", [16, 14]),
+                ("A", [2, 0]),
+                ("T", [2, 5]),
+                ("C", [3, 0]),
+                ("G", [4, 3]),
+                ("Insertion", ["3:AA", "1:C|2:T"]),
+                ("Deletion", [np.NaN, "3:AC"]),
+            )
+        )
+    )
+
+
+@fixture
+def empty_stranded_counts(stranded_counts):
+    return DataFrame(columns=stranded_counts.columns.values)
+
+
+@fixture
+def empty_unstranded_counts(unstranded_counts):
+    return DataFrame(columns=unstranded_counts.columns.values)
+
+
+def test_destrand_counts_doesnt_raise_error_for_empty_df(
+    empty_stranded_counts, empty_unstranded_counts
+):
+    assert destrand_counts(empty_stranded_counts).equals(
+        empty_unstranded_counts
+    )
+
+
+def test_destrand_counts(stranded_counts, unstranded_counts):
+    assert destrand_counts(stranded_counts).equals(unstranded_counts)
+
+
+@mark.parametrize(
+    "input,expected",
+    [
+        ("2:a", "2:A"),
+        ("2:A|2:a", "4:A"),
+        (np.NaN, np.NaN),
+        ("2:AC|1:A", "2:AC|1:A"),
+    ],
+)
+def test_combine_indel_counts(input, expected):
+    np.testing.assert_equal(combine_indel_counts(input), expected)
+
+
+def test_find_shared_bases_with_one_below_cutoff():
+    counts = [
+        DataFrame(
+            OrderedDict(
+                (
+                    ("chr", ["scaf1"]),
+                    ("loc", [1]),
+                    ("ref", ["A"]),
+                    ("depth", [20]),
+                    ("A", [10]),
+                    ("T", [10]),
+                    ("C", [0]),
+                    ("G", [0]),
+                    ("Insertion", [np.NaN]),
+                    ("Deletion", [np.NaN]),
+                )
+            )
+        ),
+        DataFrame(
+            OrderedDict(
+                (
+                    ("chr", ["scaf1"]),
+                    ("loc", [1]),
+                    ("ref", ["A"]),
+                    ("depth", [20]),
+                    ("A", [18]),
+                    ("T", [2]),
+                    ("C", [0]),
+                    ("G", [0]),
+                    ("Insertion", [np.NaN]),
+                    ("Deletion", [np.NaN]),
+                )
+            )
+        ),
+    ]
+    assert list(find_shared_bases(counts, "T")) == [True]
+
+
+def test_find_shared_bases_with_all_above_cutoff():
+    counts = [
+        DataFrame(
+            OrderedDict(
+                (
+                    ("chr", ["scaf1"]),
+                    ("loc", [1]),
+                    ("ref", ["A"]),
+                    ("depth", [20]),
+                    ("A", [10]),
+                    ("T", [10]),
+                    ("C", [0]),
+                    ("G", [0]),
+                    ("Insertion", [np.NaN]),
+                    ("Deletion", [np.NaN]),
+                )
+            )
+        ),
+        DataFrame(
+            OrderedDict(
+                (
+                    ("chr", ["scaf1"]),
+                    ("loc", [1]),
+                    ("ref", ["A"]),
+                    ("depth", [20]),
+                    ("A", [10]),
+                    ("T", [10]),
+                    ("C", [0]),
+                    ("G", [0]),
+                    ("Insertion", [np.NaN]),
+                    ("Deletion", [np.NaN]),
+                )
+            )
+        ),
+    ]
+    assert list(find_shared_bases(counts, "T")) == [True]
+
+
+def test_find_shared_bases_with_unique_allele():
+    counts = [
+        DataFrame(
+            OrderedDict(
+                (
+                    ("chr", ["scaf1"]),
+                    ("loc", [1]),
+                    ("ref", ["A"]),
+                    ("depth", [20]),
+                    ("A", [10]),
+                    ("T", [10]),
+                    ("C", [0]),
+                    ("G", [0]),
+                    ("Insertion", [np.NaN]),
+                    ("Deletion", [np.NaN]),
+                )
+            )
+        ),
+        DataFrame(
+            OrderedDict(
+                (
+                    ("chr", ["scaf1"]),
+                    ("loc", [1]),
+                    ("ref", ["A"]),
+                    ("depth", [20]),
+                    ("A", [20]),
+                    ("T", [0]),
+                    ("C", [0]),
+                    ("G", [0]),
+                    ("Insertion", [np.NaN]),
+                    ("Deletion", [np.NaN]),
+                )
+            )
+        ),
+    ]
+    assert list(find_shared_bases(counts, "T")) == [False]
+
+
+def test_find_shared_bases_with_all_zero():
+    counts = [
+        DataFrame(
+            OrderedDict(
+                (
+                    ("chr", ["scaf1"]),
+                    ("loc", [1]),
+                    ("ref", ["A"]),
+                    ("depth", [0]),
+                    ("A", [0]),
+                    ("T", [0]),
+                    ("C", [0]),
+                    ("G", [0]),
+                    ("Insertion", [np.NaN]),
+                    ("Deletion", [np.NaN]),
+                )
+            )
+        ),
+        DataFrame(
+            OrderedDict(
+                (
+                    ("chr", ["scaf1"]),
+                    ("loc", [1]),
+                    ("ref", ["A"]),
+                    ("depth", [0]),
+                    ("A", [0]),
+                    ("T", [0]),
+                    ("C", [0]),
+                    ("G", [0]),
+                    ("Insertion", [np.NaN]),
+                    ("Deletion", [np.NaN]),
+                )
+            )
+        ),
+    ]
+    assert list(find_shared_bases(counts, "T")) == [False]
+
+
+def test_find_shared_indels_with_one_below_cutoff():
+    counts = [
+        DataFrame(
+            OrderedDict(
+                (
+                    ("chr", ["scaf1"]),
+                    ("loc", [1]),
+                    ("ref", ["A"]),
+                    ("depth", [20]),
+                    ("A", [10]),
+                    ("T", [0]),
+                    ("C", [0]),
+                    ("G", [0]),
+                    ("Insertion", ["10:A"]),
+                    ("Deletion", [np.NaN]),
+                )
+            )
+        ),
+        DataFrame(
+            OrderedDict(
+                (
+                    ("chr", ["scaf1"]),
+                    ("loc", [1]),
+                    ("ref", ["A"]),
+                    ("depth", [20]),
+                    ("A", [19]),
+                    ("T", [0]),
+                    ("C", [0]),
+                    ("G", [0]),
+                    ("Insertion", ["1:A"]),
+                    ("Deletion", [np.NaN]),
+                )
+            )
+        ),
+    ]
+    assert list(find_shared_indels(counts, "Insertion")) == [["A"]]
+
+
+def test_find_shared_indels_with_all_above_cutoff():
+    counts = [
+        DataFrame(
+            OrderedDict(
+                (
+                    ("chr", ["scaf1"]),
+                    ("loc", [1]),
+                    ("ref", ["A"]),
+                    ("depth", [20]),
+                    ("A", [10]),
+                    ("T", [0]),
+                    ("C", [0]),
+                    ("G", [0]),
+                    ("Insertion", ["10:A"]),
+                    ("Deletion", [np.NaN]),
+                )
+            )
+        ),
+        DataFrame(
+            OrderedDict(
+                (
+                    ("chr", ["scaf1"]),
+                    ("loc", [1]),
+                    ("ref", ["A"]),
+                    ("depth", [20]),
+                    ("A", [10]),
+                    ("T", [0]),
+                    ("C", [0]),
+                    ("G", [0]),
+                    ("Insertion", ["10:A"]),
+                    ("Deletion", [np.NaN]),
+                )
+            )
+        ),
+    ]
+    assert find_shared_indels(counts, "Insertion").to_dict() == {0: ["A"]}
+
+
+def test_find_shared_indels_with_unique_allele():
+    counts = [
+        DataFrame(
+            OrderedDict(
+                (
+                    ("chr", ["scaf1"]),
+                    ("loc", [1]),
+                    ("ref", ["A"]),
+                    ("depth", [20]),
+                    ("A", [10]),
+                    ("T", [0]),
+                    ("C", [0]),
+                    ("G", [0]),
+                    ("Insertion", ["10:A"]),
+                    ("Deletion", [np.NaN]),
+                )
+            )
+        ),
+        DataFrame(
+            OrderedDict(
+                (
+                    ("chr", ["scaf1"]),
+                    ("loc", [1]),
+                    ("ref", ["A"]),
+                    ("depth", [20]),
+                    ("A", [20]),
+                    ("T", [0]),
+                    ("C", [0]),
+                    ("G", [0]),
+                    ("Insertion", [np.NaN]),
+                    ("Deletion", [np.NaN]),
+                )
+            )
+        ),
+    ]
+    assert find_shared_indels(counts, "Insertion").to_dict() == dict()
+
+
+def test_find_shared_indels_with_all_zero():
+    counts = [
+        DataFrame(
+            OrderedDict(
+                (
+                    ("chr", ["scaf1"]),
+                    ("loc", [1]),
+                    ("ref", ["A"]),
+                    ("depth", [0]),
+                    ("A", [0]),
+                    ("T", [0]),
+                    ("C", [0]),
+                    ("G", [0]),
+                    ("Insertion", [np.NaN]),
+                    ("Deletion", [np.NaN]),
+                )
+            )
+        ),
+        DataFrame(
+            OrderedDict(
+                (
+                    ("chr", ["scaf1"]),
+                    ("loc", [1]),
+                    ("ref", ["A"]),
+                    ("depth", [0]),
+                    ("A", [0]),
+                    ("T", [0]),
+                    ("C", [0]),
+                    ("G", [0]),
+                    ("Insertion", [np.NaN]),
+                    ("Deletion", [np.NaN]),
+                )
+            )
+        ),
+    ]
+    assert find_shared_indels(counts, "Insertion").to_dict() == dict()
+
+
+def test_find_shared_indels_with_multiple_shared():
+    counts = [
+        DataFrame(
+            OrderedDict(
+                (
+                    ("chr", ["scaf1"]),
+                    ("loc", [1]),
+                    ("ref", ["A"]),
+                    ("depth", [20]),
+                    ("A", [0]),
+                    ("T", [0]),
+                    ("C", [0]),
+                    ("G", [0]),
+                    ("Insertion", ["10:A|10:AA"]),
+                    ("Deletion", [np.NaN]),
+                )
+            )
+        ),
+        DataFrame(
+            OrderedDict(
+                (
+                    ("chr", ["scaf1"]),
+                    ("loc", [1]),
+                    ("ref", ["A"]),
+                    ("depth", [20]),
+                    ("A", [0]),
+                    ("T", [0]),
+                    ("C", [0]),
+                    ("G", [0]),
+                    ("Insertion", ["10:A|10:AA"]),
+                    ("Deletion", [np.NaN]),
+                )
+            )
+        ),
+    ]
+    assert set(find_shared_indels(counts, "Insertion").to_dict()[0]) == set(
+        ["AA", "A"]
+    )
+
+
+def test_find_shared_indels_with_multiple_unique():
+    counts = [
+        DataFrame(
+            OrderedDict(
+                (
+                    ("chr", ["scaf1"]),
+                    ("loc", [1]),
+                    ("ref", ["A"]),
+                    ("depth", [20]),
+                    ("A", [0]),
+                    ("T", [0]),
+                    ("C", [0]),
+                    ("G", [0]),
+                    ("Insertion", ["10:A|10:AA"]),
+                    ("Deletion", [np.NaN]),
+                )
+            )
+        ),
+        DataFrame(
+            OrderedDict(
+                (
+                    ("chr", ["scaf1"]),
+                    ("loc", [1]),
+                    ("ref", ["A"]),
+                    ("depth", [20]),
+                    ("A", [20]),
+                    ("T", [0]),
+                    ("C", [0]),
+                    ("G", [0]),
+                    ("Insertion", [np.NaN]),
+                    ("Deletion", [np.NaN]),
+                )
+            )
+        ),
+    ]
+    assert find_shared_indels(counts, "Insertion").to_dict() == dict()
