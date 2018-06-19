3cd5e28d98c263e842f36202d7161d860f63fdcf
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-06-19 10:22:57 -0700

    Extended flanking base extraction to allow one-sided extraction

--- bin/extract_flanking_bases.py
+++ bin/extract_flanking_bases.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
+from collections import OrderedDict
 import os
+import sys
 from tempfile import NamedTemporaryFile
 from uuid import uuid4
 
@@ -14,18 +16,30 @@ from plumbum.cmd import bedtools
 @click.option(
     "-n", "--num-flanking", type=int, help="Number of flanking bases to extract"
 )
+@click.option(
+    "--pre", type=int, help="Number of bases before variant to extract"
+)
+@click.option(
+    "--post", type=int, help="Number of bases after variant to extract"
+)
 @click.argument("tsv", nargs=1)
-def extract_flanking_bases(reference, num_flanking, tsv):
+def extract_flanking_bases(reference, pre, post, num_flanking, tsv):
     """Extract flanking sequence from a variant table."""
+    if sum([bool(pre), bool(post), bool(num_flanking)]) > 1:
+        sys.exit("Only one of -pre/-post/-n should be defined")
     variants = read_csv(tsv, sep="\t")
+    if num_flanking:
+        r1 = variants["POS"] - num_flanking
+        r2 = variants["POS"] + num_flanking
+    elif pre:
+        r1 = variants["POS"] - pre
+        r2 = variants["POS"]
+    elif post:
+        r1 = variants["POS"]
+        r2 = variants["POS"] + post
     bedfile_contents = DataFrame(
-        {
-            "CHROM": variants["CHROM"],
-            "r1": variants["POS"] - num_flanking,
-            "r2": variants["POS"] + num_flanking,
-        }
+        OrderedDict([("CHROM", variants["CHROM"]), ("r1", r1), ("r2", r2)])
     )
-    bedfile_contents = bedfile_contents[["CHROM", "r1", "r2"]]
     tmpfile = os.path.join("/tmp", uuid4().hex, "flanking.bed")
     with NamedTemporaryFile() as tmpfile:
         bedfile_contents.to_csv(
